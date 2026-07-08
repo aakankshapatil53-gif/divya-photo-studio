c0aeeeec13e5406253f8b7b922e8f2c96cbcb6b1
Aakanksha Patil <aakankshapatil53@gmail.com>
2026-07-08 19:20:40 +0530

    Update seed.ts

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -5,8 +5,8 @@ import "dotenv/config";
 const prisma = new PrismaClient();
 
 async function main() {
-  const username = process.env.ADMIN_USERNAME || "admin";
-  const password = process.env.ADMIN_PASSWORD || "ChangeMe123!";
+  const username = process.env.ADMIN_USERNAME || "SHARAD";
+  const password = process.env.ADMIN_PASSWORD || "Divya@123!";
   const passwordHash = await bcrypt.hash(password, 10);
 
   await prisma.admin.upsert({
